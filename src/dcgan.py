54b68c7f4d4c4301cbedc415e873968682c468e0
DavidKo3 <automorphicnew@gmail.com>
2018-04-16 07:06:29 +0900

    add dcgan generator

--- src/dcgan.py
+++ src/dcgan.py
@@ -10,6 +10,34 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
+class G(nn.module):
+    def __init__(self, d=128):
+        super(G, self).__init__()
+        self.deconv1 = nn.ConvTranspose2d(100, d*8, 4, 1, 0)
+        self.deconv1_bn = nn.BatchNorm2d(d*8)
+        self.deconv2 = nn.ConvTranspose2d(d*8, d*4, 4, 2, 1)
+        self.deconv2_bn = nn.BatchNorm2d(d*4)
+        self.deconv3 = nn.ConvTranspose2d(d*4, d*2, 4, 2, 1)
+        self.deconv3_bn = nn.BatchNorm2d(d*2)
+        self.deconv4 = nn.ConvTranspose2d(d*2, d, 4, 2, 1)
+        self.deconv4_bn = nn.BatchNorm2d(d)
+        self.deconv5 = nn.ConvTranspose2d(d, 3, 4, 2, 1)
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        x = F.relu(self.deconv1_bn(self.deconv1(input)))
+        x = F.relu(self.deconv2_bn(self.deconv2(x)))
+        x = F.relu(self.deconv3_bn(self.deconv3(x)))
+        x = F.relu(self.deconv4_bn(self.deconv4(x)))
+        x = F.tanh(self.deconv5(x))
+
+        return x
+
+
+
 
 class D(nn.module):
     def __init__(self, d=128):
@@ -42,4 +70,3 @@ def normal_init(m, mean, std):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
         m.weight.data.normal_(mean, std)
         m.bias.data.zero_()
-        
