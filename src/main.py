4c89b177c78d314c958fd4cb205a44486c294270
DavidKo3 <automorphicnew@gmail.com>
2018-05-13 20:15:05 +0900

    add result and train histogram

--- src/main.py
+++ src/main.py
@@ -58,8 +58,113 @@ transform = transforms.Compose([
         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
 ])
 
-fixed_z = torch.randn((5*5, 100)).view(-1, 100, 1, 1)   # fixed noise
-fixed_z = Variable(fixed_z.cuda(), volatile=True)
+
+fixed_z_ = torch.randn((5 * 5, 100)).view(-1, 100, 1, 1)    # fixed noise
+fixed_z_ = Variable(fixed_z_.cuda(), volatile=True)
+
+
+def show_result(num_epoch, show = False, save = False, path = 'result.png', isFix=False):
+    z_ = torch.randn((5*5, 100)).view(-1, 100, 1, 1)
+    z_ = Variable(z_.cuda(), volatile=True)
+
+    G.eval()
+    if isFix:
+        test_images = G(fixed_z_)
+    else:
+        test_images = G(z_)
+    G.train()
+
+    size_figure_grid = 5
+    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(5, 5))
+    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
+        ax[i, j].get_xaxis().set_visible(False)
+        ax[i, j].get_yaxis().set_visible(False)
+
+    for k in range(5*5):
+        i = k // 5
+        j = k % 5
+        ax[i, j].cla()
+        ax[i, j].imshow((test_images[k].cpu().data.numpy().transpose(1, 2, 0) + 1) / 2)
+
+    label = 'Epoch {0}'.format(num_epoch)
+    fig.text(0.5, 0.04, label, ha='center')
+    plt.savefig(path)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
+    x = range(len(hist['D_losses']))
+
+    y1 = hist['D_losses']
+    y2 = hist['G_losses']
+
+    plt.plot(x, y1, label='D_loss')
+    plt.plot(x, y2, label='G_loss')
+
+    plt.xlabel('Iter')
+    plt.ylabel('Loss')
+
+    plt.legend(loc=4)
+    plt.grid(True)
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(path)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+    fixed_z_ = torch.randn((5 * 5, 100)).view(-1, 100, 1, 1)  # fixed noise
+    fixed_z_ = Variable(fixed_z_.cuda(), volatile=True)
+
+    def show_result(num_epoch, show=False, save=False, path='result.png', isFix=False):
+        z_ = torch.randn((5 * 5, 100)).view(-1, 100, 1, 1)
+        z_ = Variable(z_.cuda(), volatile=True)
+
+        G.eval()
+        if isFix:
+            test_images = G(fixed_z_)
+        else:
+            test_images = G(z_)
+        G.train()
+
+        size_figure_grid = 5
+        fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(5, 5))
+        for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
+            ax[i, j].get_xaxis().set_visible(False)
+            ax[i, j].get_yaxis().set_visible(False)
+
+        for k in range(5 * 5):
+            i = k // 5
+            j = k % 5
+            ax[i, j].cla()
+            ax[i, j].imshow((test_images[k].cpu().data.numpy().transpose(1, 2, 0) + 1) / 2)
+
+        label = 'Epoch {0}'.format(num_epoch)
+        fig.text(0.5, 0.04, label, ha='center')
+        plt.savefig(path)
+
+        if show:
+            plt.show()
+        else:
+            plt.close()
+
+
+
+
+
+
+
+
+
+
+
 
 
 # training parameters
@@ -96,6 +201,15 @@ BCE_loss = nn.BCELoss()
 G_optimizer = optim.Adam(G.parameters(), lr=lr, betas=(0.5, 0.999))
 D_optimizer = optim.Adam(D.parameters(), lr=lr, betas=(0.5, 0.999))
 
+train_hist = {}
+train_hist['D_losses'] = []
+train_hist['G_losses'] = []
+train_hist['per_epoch_ptimes'] = []
+train_hist['total_ptime'] = []
+
+
+print("=*10")
+print("training begin !!")
 
 start_time = time.time()
 for epoch in range(train_epoch):
@@ -134,16 +248,26 @@ for epoch in range(train_epoch):
         D_train_loss.backward()
         D_optimizer.step()
 
-
-        
-
-
+        D_losses.append(D_train_loss.data[0])
 
 
         ############################
         # (2) Update G network: maximize log(D(G(z)))
         ###########################
         # train generator G
+        G.zero_grad()
+
+        z = torch.randn((mini_batch, 100)).view(-1, 100, 1 ,1) # [mini_batch x 100] - > [mini_batch x 100 x 1 x 1]
+        z = Variable(z.cuda())
+
+        G_result = G(z)
+        G_result = D(G_result).squeeze()
+        G_train_loss = BCE_loss(D_result, y_real)
+        G_train_loss.backward()
+        G_train_loss.step()
+
+        G_losses.append(G_train_loss.data[0])
+
 
 
 
